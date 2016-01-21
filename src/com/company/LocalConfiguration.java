e2362e5da4de51c52db4c64a8f6ad80628d82dbe
GooSP <tonyhennequin@hotmail.fr>
2016-01-21 11:44:46 +0100

    delete local

--- src/com/company/LocalConfiguration.java
+++ src/com/company/LocalConfiguration.java
@@ -1,8 +1,5 @@
 package com.company;
 
-/**
- * Created by Gosp-enssat on 21/01/2016.
- */
 public class LocalConfiguration {
     public static final String Folder = "C://Users/Gosp-enssat/Desktop/English/Img/GoodImg/Bmp/";
 }

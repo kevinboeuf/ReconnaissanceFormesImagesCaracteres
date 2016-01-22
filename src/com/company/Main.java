f9568ea2d7baab5a93355d94338075157a7268c4
nightswolfs <kboeuf@gmail.com>
2016-01-22 12:29:49 +0100

    Extraction d'attributs des images

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
+import java.awt.image.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,15 +24,18 @@ public class Main {
 
         //Get the list of images
         List<ImageRelation> imageRelationAttributesList = new ArrayList<ImageRelation>();
-        HashMap <String, Image> imagesList = getImagesList(1, 62);
+        HashMap <String, Image> imagesList = getImagesList(12, 50);
 
         System.out.println("\t" + imagesList.size() + " images récupérées");
 
         //Show the images
-        /*initDrawingFrame();
-        for (Map.Entry<ImageClass, BufferedImage> entry : imagesList.entrySet()){
-            showBufferedImage(entry.getKey(), entry.getValue());
-        }*/
+        initDrawingFrame();
+        for (Map.Entry<String, Image> entry : imagesList.entrySet()){
+            //showBufferedImage(entry.getKey(), entry.getValue().bufferedImage);
+            //getBlackWhiteImage(entry.getKey(), entry.getValue().bufferedImage);
+            //getGaussianBluredImage(entry.getKey(), entry.getValue().bufferedImage);
+            getBlackWhiteImage(entry.getKey(), getGaussianBluredImage(entry.getKey(), entry.getValue().bufferedImage));
+        }
 
         BufferedImage image;
 
@@ -115,6 +118,9 @@ public class Main {
         imagePanel.repaint();
     }
 
+    /**
+     * Initialize the drawing frame
+     */
     public static void initDrawingFrame(){
         jframe.setSize(250, 250);
         jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -122,5 +128,87 @@ public class Main {
         pane = jframe.getContentPane();
         pane.add(imagePanel);
         jframe.setVisible(true);
+    }
+
+    /*public static void getBlackWhiteImage(String name, BufferedImage image) {
+        BufferedImage grayScale = image;
+        ColorConvertOp op = new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
+        BufferedImage res = op.filter(grayScale, grayScale);
+
+        BufferedImage blackWhite = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+        showBufferedImage(name, res);
+    }*/
+
+    public static BufferedImage getBlackWhiteImage(String name, BufferedImage image){
+        BufferedImage original = image;
+        BufferedImage binarized = new BufferedImage(original.getWidth(), original.getHeight(),BufferedImage.TYPE_BYTE_BINARY);
+        try
+        {
+
+            int red;
+            int newPixel;
+            int threshold = 100;
+
+            for(int i=0; i<original.getWidth(); i++)
+            {
+                for(int j=0; j<original.getHeight(); j++)
+                {
+                    // Get pixels
+                    red = new Color(original.getRGB(i, j)).getGreen();
+                    int alpha = new Color(original.getRGB(i, j)).getAlpha();
+                    if(red > threshold)
+                    {
+                        newPixel = 0;
+                    }
+                    else
+                    {
+                        newPixel = 255;
+                    }
+                    newPixel = colorToRGB(alpha, newPixel, newPixel, newPixel);
+                    binarized.setRGB(i, j, newPixel);
+                }
+            }
+            ImageIO.write(binarized, "png",new File("blackwhiteimage") );
+            showBufferedImage(name, binarized);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return binarized;
+    }
+
+    public static int colorToRGB(int alpha, int red, int green, int blue) {
+        int newPixel = 0;
+        newPixel += alpha;
+        newPixel = newPixel << 8;
+        newPixel += red; newPixel = newPixel << 8;
+        newPixel += green; newPixel = newPixel << 8;
+        newPixel += blue;
+
+        return newPixel;
+    }
+
+    public static BufferedImage getGaussianBluredImage(String name, BufferedImage image){
+        float[] matrix = {
+                1/16f, 1/8f, 1/16f,
+                1/8f, 1/4f, 1/8f,
+                1/16f, 1/8f, 1/16f,
+        };
+
+        float[] matrix2 = {
+                1/273f, 4/273f, 7/273f, 4/273f, 1/273f,
+                4/273f, 16/273f, 26/273f, 16/273f, 4/273f,
+                7/273f, 26/273f, 41/273f, 26/273f, 7/273f,
+                4/273f, 16/273f, 26/273f, 16/273f, 4/273f,
+                1/273f, 4/273f, 7/273f, 4/273f, 1/273f,
+        };
+
+        BufferedImage original = image;
+        BufferedImageOp op = new ConvolveOp( new Kernel(3, 3, matrix) );
+        BufferedImage blurredImage = op.filter(original, null);
+
+        //showBufferedImage(name, blurredImage);
+        return blurredImage;
     }
 }
